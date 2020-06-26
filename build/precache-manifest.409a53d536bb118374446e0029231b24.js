246c48e8810b2bbe98f9fe5cd497da818463ccfd
Rob Mason <robertddmason@gmail.com>
2020-06-26 12:48:28 -0700

    run build w/ changes

---
+++ build/precache-manifest.409a53d536bb118374446e0029231b24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb5668a977d075ea5af34893e17c98a",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "3696960fb136974596cd",
+    "url": "/User-Directory/static/css/main.a4cd29b7.chunk.css"
+  },
+  {
+    "revision": "98d381b8539990c9da13",
+    "url": "/User-Directory/static/js/2.88f9d077.chunk.js"
+  },
+  {
+    "revision": "3696960fb136974596cd",
+    "url": "/User-Directory/static/js/main.d54c3224.chunk.js"
+  },
+  {
+    "revision": "1a48e735230d9091ff90",
+    "url": "/User-Directory/static/js/runtime~main.2facd29f.js"
+  }
+]);
